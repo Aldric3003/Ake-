81d7e4ca0a56ac451f75ef1502ed5f80e6fb0365
@aldric3003 <aldric3003mercenary@gmail.com>
2025-01-13 13:03:46 +0100

    denrele

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+    const sidebar = document.getElementById("sidebar");
+    const menuToggle = document.getElementById("menuToggle").querySelector("span");
+
+
+    if (sidebar.style.left === "0px") {
+        sidebar.style.left = "-250px";
+        menuToggle.innerHTML = "&#9776;"; 
+    } else {
+        sidebar.style.left = "0px";
+        menuToggle.innerHTML = "&times;";
+    }
+}
+
+
